3fc2a17ac57fcdc3fdb32048406a8a2e752ad632
Ashish <ashishpanghal006@gmail.com>
2025-08-09 06:06:46 +0530

    modified footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
             </div>
         </div>
         <p className="pt-4 text-center text-xs md:text-sm pb-5">
-            Copyright 2025 © PixlyAI. All Right Reserved.
+            Copyright 2025 © PixlyAi. All Right Reserved.
         </p>
     </footer>
   )
